503a86880476a2eec29d019b413db1b4ad9df372
Yan  Batista <y.batista@gsuite.iff.edu.br>
2023-01-17 21:25:22 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import GlobalStyle from "./styles/global";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Home</h1>
+
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
